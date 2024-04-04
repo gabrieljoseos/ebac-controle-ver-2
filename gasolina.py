3726cc0760567c9d89e227be0d49fc0e20425216
gabrieljoseos <gabriel.jos@hotmail.com>
2024-04-04 13:52:15 +0000

    Arquivos alterados branch develop

--- gasolina.py
+++ gasolina.py
@@ -2,12 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# código de geração do gráfico
 gasolina_df = pd.read_csv("gasolina.csv")
+gasolina = sns.lineplot(data=gasolina_df, x="dia", y="venda", color='red')
+gasolina.set(title="Valor da Gasolina em reais durante 10 dias", xlabel="Dias", ylabel="Valor(R$)")
 
-gasolina_df.head(10)
-
-gasolina = sns.lineplot(data=gasolina_df, x="dia", y="venda", color='blue')
-gasolina.set(title="Valor da Gasolina durante 10 dias", xlabel="Dia", ylabel="Valor")
-
+#salvar o grafico como png
 gasolina_fig = gasolina.get_figure()
 gasolina_fig.savefig('gasolina.png')
